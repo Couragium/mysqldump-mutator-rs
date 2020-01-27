89ef189a4b530466f8fa0c2795ee54bc0db2f46b
David Bonet <webbonet@gmail.com>
2020-01-27 19:48:10 +0100

    update crate name

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use sqlparser::Parser;
-use sqlparser::Token;
+use mysqldump_mutator::{Parser, Token};
+use std::io::Write;
 
 use std::fs::File;
 use std::io::{self, BufReader};
@@ -7,15 +7,15 @@ use std::io::{self, BufReader};
 fn main() -> io::Result<()> {
     env_logger::init();
 
-    let f = File::open("/home/david/Documents/Omnea/dump_custarea.sql")?;
+    let args: Vec<String> = std::env::args().collect();
 
     let (mut count_a, mut count_b) = (0, 0);
 
     let result = Parser::parse_mysqldump(
-        BufReader::new(f),
+        BufReader::new(File::open(&args[1])?),
         |_context, token| {
             //println!("{:?} {}", context, token);
-            Token::new(&format!("Hola{}", count_a), Some('\''));
+            Token::new(&format!("REDACTED{}", count_a), Some('\''));
             //Token::Word(Word {value: "42".to_string(), quote_style: None, keyword: "".to_string()})
             count_a += 1;
             token
@@ -29,6 +29,13 @@ fn main() -> io::Result<()> {
     );
 
     if let Err(result) = result {
+        let stderr = io::stderr();
+        let mut handle = stderr.lock();
+
+        handle.write_all(
+            format!("\nError found during the file processing: {}\n", result).as_ref(),
+        )?;
+
         println!("{:?}", result);
     }
 
